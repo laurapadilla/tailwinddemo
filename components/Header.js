723c87200910d281d465baea8e54ca319ac8d540
Laura Padilla <hi@lauracodes.com>
2021-10-29 14:03:54 -0400

    Add reset css

---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const TextLink = styled.a`
+  border-left: 1px solid black;
+  padding: 1rem;
+`;
+
+export const LinkBtn = styled(TextLink)`
+  background: black;
+  color: white;
+`;
+
+export const Header = () => {
+  return (
+    <header className="w-full fixed flex justify-between items-center border-b border-gray-900">
+      <h1 className="p-4">Off the Rook</h1>
+      <nav>
+        <TextLink href="/">Pieces</TextLink>
+        <TextLink href="/">Openings</TextLink>
+        <TextLink href="/">Players</TextLink>
+        <LinkBtn href="/">Learn Chess →</LinkBtn>
+      </nav>
+    </header>
+  );
+};
---
+++ components/Quote.js
@@ -0,0 +1,10 @@
+export const Quote = () => {
+  return (
+    <div className="container mx-auto my-auto text-center font-light">
+      <h1 className="text-5xl mb-4">
+        I don’t believe in psychology. I believe in good moves.
+      </h1>
+      <h4 className="text-2xl">Bobby Fischer</h4>
+    </div>
+  );
+};
